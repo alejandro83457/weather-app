69aa937e34410897bae7a1172024787fb1d3f17d
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-27 21:26:18 -0800

    Add simple weather fetch

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('test');
+const API_KEY = 'ba3fc19bd3454083abe43519242601';
+const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Calexico`;
+
+async function getWeatherData() {
+  const response = await fetch(url);
+  const weatherData = await response.json();
+  console.log(weatherData);
+}
+
+getWeatherData();
