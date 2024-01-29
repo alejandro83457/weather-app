0b1ebdb18e8e72384187c019d6c7f23b9a034db1
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-29 15:34:43 -0800

    Log relevant weather metrics on search

--- src/index.js
+++ src/index.js
@@ -1,10 +1,30 @@
 const API_KEY = 'ba3fc19bd3454083abe43519242601';
-const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Calexico`;
 
-async function getWeatherData() {
+async function getWeatherData(loc) {
+  const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${loc}`;
   const response = await fetch(url);
   const weatherData = await response.json();
-  console.log(weatherData);
+  addWeatherData(weatherData);
 }
 
-getWeatherData();
+function addWeatherData(data) {
+  const weatherDiv = document.querySelector('#weather');
+  const subDiv1 = document.createElement('div');
+
+  const { name, region } = data.location;
+  const { text, icon } = data.current.condition;
+  const { temp_f, temp_c } = data.current;
+
+  console.log(data);
+  console.log(name, region, text, temp_f, temp_c);
+}
+
+function addFindListener() {
+  const find = document.querySelector('#find');
+  const input = document.querySelector('#location');
+  find.addEventListener('click', () => {
+    getWeatherData(input.value);
+  });
+}
+
+addFindListener();
