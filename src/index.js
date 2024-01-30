83d6137e141df81cb418065060a839cc53c5c9cf
alejandro83457 <martinezalejandro2001@hotmail.com>
2024-01-29 22:15:53 -0800

    Add error handling for display

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const API_KEY = 'ba3fc19bd3454083abe43519242601';
 
+// adds weather data to DOM
 function addWeatherData(data) {
   const weatherDiv = document.querySelector('#weather');
 
@@ -12,6 +13,7 @@ function addWeatherData(data) {
   const tempDiv = document.createElement('div');
   const img = document.createElement('img');
 
+  mainDiv.id = 'main-div';
   locDiv.id = 'location-div';
   tempDiv.id = 'temp-div';
   textDiv.id = 'text-div';
@@ -35,23 +37,54 @@ function addWeatherData(data) {
 
   weatherDiv.appendChild(mainDiv);
 
-  console.log(data);
-  console.log(name, region, text, temp_f);
+  // console.log(data);
 }
 
+// shows error on dom instead of weather data
+function handleError(code) {
+  let errorMessage;
+  const weatherDiv = document.querySelector('#weather');
+  const errorDiv = document.createElement('div');
+
+  // remove child if present
+  if (weatherDiv.firstChild) weatherDiv.removeChild(weatherDiv.firstChild);
+
+  if (code === 403) {
+    errorMessage = '403 Error. Possibly wrong API key.';
+  } else if (code === 400) {
+    errorMessage = '400 Error. Invalid input.';
+  } else {
+    errorMessage = 'Something went wrong.';
+  }
+
+  errorDiv.textContent = errorMessage;
+  errorDiv.style.fontSize = '50px';
+  weatherDiv.appendChild(errorDiv);
+}
+
+// fetches weather data
 async function getWeatherData(loc) {
   const url = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${loc}`;
   const response = await fetch(url);
+
+  // print error and return;
+  if (!response.ok) {
+    handleError(response.status);
+    return;
+  }
+
   const weatherData = await response.json();
   addWeatherData(weatherData);
 }
 
+// adds event listener to find button
 function addFindListener() {
   const find = document.querySelector('#find');
   const input = document.querySelector('#location');
   find.addEventListener('click', () => {
-    getWeatherData(input.value);
+    if (input.value.length > 0) getWeatherData(input.value);
   });
 }
 
+// init call
 addFindListener();
